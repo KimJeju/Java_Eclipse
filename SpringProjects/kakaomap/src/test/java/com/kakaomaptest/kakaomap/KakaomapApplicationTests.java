0a7ac9bcbfcf2ec5a28ad6945874b02e38c036e5
kingswingsex <kingswingsex@gmail.com>
2022-09-17 14:34:17 +0900

    스프링 맵 테스트

---
+++ SpringProjects/kakaomap/src/test/java/com/kakaomaptest/kakaomap/KakaomapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaomaptest.kakaomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
